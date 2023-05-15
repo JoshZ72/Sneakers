dc23dda8ee612ef6d58ac2e8d5a061278cbe7b05
tokenflipguy <126824276+tokenflipguy@users.noreply.github.com>
2023-05-14 20:28:30 -0700

    Added Credits Music Story Opening and  Title Music
    Initial Credits, Music, and Story Intro linked up.

---
+++ Sneakers/Assets/Scripts/Menu/StoryScreenLoader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryScreenLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+
+        //Load next scrreen
+        SceneManager.LoadScene("Sam Scene", LoadSceneMode.Single);
+        
+    }
+
+   
+}
