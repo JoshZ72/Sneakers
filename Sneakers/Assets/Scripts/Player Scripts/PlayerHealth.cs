9d132f87cfa4471173a84903728db77b2e604d69
SamlSteffon <126824407+SamlSteffon@users.noreply.github.com>
2023-05-16 00:51:20 -0700

    Revert "Revert "Merge branch 'main' of https://github.com/JoshZ72/Sneakers""
    This reverts commit b4402188864be7f0214d3dfdb135b903bb96b5b6.

--- Sneakers/Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ Sneakers/Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -33,6 +33,10 @@ public class PlayerHealth : MonoBehaviour
         {
             //SceneManager.LoadScene("cRED");
         }
+        if (quests.goal.IsReached())
+        {
+            quests.Complete();
+        }
 
     }
 
